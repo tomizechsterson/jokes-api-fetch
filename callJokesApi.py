525788d9be79a1dba2888c9109c8f75d682f11df
Tom <tomizechsterson@gmail.com>
2017-08-01 21:15:59 -0700

    Initial Commit

---
+++ callJokesApi.py
@@ -0,0 +1,11 @@
+import requests
+import html
+
+response = requests.get('https://api.icndb.com/jokes/random')
+# TODO: Add the ability to pass params for filter, exclude, first/last name and id
+# TODO: Go TDD with this
+if response.ok:
+    json = response.json()
+    print(html.unescape(json['value']['joke']))
+else:
+    response.raise_for_status()
