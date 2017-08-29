17b11c84546bfd3ce73fb7b19ed36f5dbf362552
Tom <tomizechsterson@gmail.com>
2017-08-28 20:31:58 -0700

    Finally working

--- callJokesApi.py
+++ callJokesApi.py
@@ -13,23 +13,36 @@ parser.add_argument("-e", "--exclude", choices=(["explicit", "nerdy"]), action="
                                                                                               "exclude. Exactly the "
                                                                                               "opposite of the "
                                                                                               "include argument")
-parser.add_argument("-f", "--firstname", help="The first name to use to replace Chuck in the returned joke")
-parser.add_argument("-l", "--lastname", help="The last name to use to replace Norris in the returned joke")
+parser.add_argument("-f", "--firstName", help="The first name to use to replace Chuck in the returned joke")
+parser.add_argument("-l", "--lastName", help="The last name to use to replace Norris in the returned joke")
 args = parser.parse_args()
 for arg in vars(args):
     print(arg, getattr(args, arg))
 
-response = requests.get('https://api.icndb.com/jokes/random')
-# TODO: Make use of the rest of the above arguments
-# firstname: firstName
-# lastname: lastName
-# include: limitTo (array)
-# exclude: exclude (array)
+url = "https://api.icndb.com/jokes/random"
+params = {}
+
+for arg in vars(args):
+    if getattr(args, arg) is not None and arg != "showid":
+        params[arg] = getattr(args, arg)
+
+if len(params) > 0:
+    url += "?"
+
+for p in params:
+    if p == "exclude":
+        url += p + "=[" + ",".join(params[p]) + "]&"
+    elif p == "include":
+        url += "limitTo=[" + ",".join(params[p]) + "]&"
+    else:
+        url += p + "=" + params[p] + "&"
+
+response = requests.get(url)
 if response.ok:
     json = response.json()
     if args.showid:
-        print(html.unescape(json['value']['joke']) + " (Joke Id: " + str(json['value']['id']) + ")")
+        print(html.unescape(json["value"]["joke"]) + " (Joke Id: " + str(json["value"]["id"]) + ")")
     else:
-        print(html.unescape(json['value']['joke']))
+        print(html.unescape(json["value"]["joke"]))
 else:
     response.raise_for_status()
